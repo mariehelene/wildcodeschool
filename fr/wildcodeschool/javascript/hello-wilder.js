7bbbfa63b97a374ba221802bfa4a7ad20f0530ba
Marie-Hélène <mariehelene.delteil@gmail.com>
2019-07-09 10:02:57 +0200

    ajout favorite.js etc...

---
+++ fr/wildcodeschool/javascript/hello-wilder.js
@@ -0,0 +1,9 @@
+/* hello-wilder.js */
+
+// Define some variables
+const myName = 'Wilder Frey';
+const myCampus = 'The Twins';
+const message = 'Hello ' + myName + '! enjoy studying JavaScript at ' + myCampus + '!';
+
+// Display a popup
+alert(message);
---
+++ fr/wildcodeschool/javascript/my-favorite.js
@@ -0,0 +1,10 @@
+/* my-favorite.js */
+
+// Define some variables
+const thePicture = 'Retour vers le Futur';
+const theYear = '1985';
+const theDirector = 'Robert Zemeckis';
+const message = thePicture + ', réalisé par ' + theDirector + ', est sorti en ' + theYear + '.';
+
+// Display a popup
+alert(message);
