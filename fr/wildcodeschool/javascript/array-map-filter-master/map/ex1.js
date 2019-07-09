a2a4208bb86f74fc83470a49374e6d81e054cd59
Marie-Hélène <mariehelene.delteil@gmail.com>
2019-07-09 18:54:16 +0200

    ex1.js

---
+++ fr/wildcodeschool/javascript/array-map-filter-master/map/ex1.js
@@ -0,0 +1,40 @@
+/* Array.prototype.map - Exercice 1
+
+Ecrire une fonction getStringsLength qui reçoit comme un argument un tableau de chaînes.
+En utilisant map, cette fonction doit renvoyer un tableau avec,
+pour chaque chaîne CHAINE de longueur N en entrée, la chaîne "CHAINE contains N characters"
+en sortie.
+
+Exemple d'entrée:
+  [
+    'Chicken',
+    'Bacon',
+    'Tofu',
+    'Mayonnaise'
+  ]
+En sortie: [
+  'Chicken contains 7 characters',
+  'Bacon contains 5 characters',
+  'Tofu contains 4 characters',
+  'Mayonnaise contains 10 characters'
+]
+
+ */
+
+function getStringsLength(strings) {
+
+  const lengths = strings.map(function(string) {
+    return {
+      string: string + " contains " + string.length + " characters"
+    };
+  });
+  return lengths;
+
+}
+
+// Si besoin, tu peux toujours visualiser le résultat avec console.log,
+// mais alors, il faut alors lancer l'exercice avec node map/ex1
+// console.log(getStringsLength(['Cat', 'Lion', 'Tiger', 'Jaguar', 'Leopard']));
+
+// Ne pas modifier l'export
+module.exports = getStringsLength;
---
+++ fr/wildcodeschool/javascript/array-map-filter-master/tests/map3.test.js
@@ -0,0 +1,52 @@
+const getMoviesFreshness = require('../map/ex3.js');
+
+xdescribe('map - exercice 3', () => {
+
+  test('getMoviesFreshness', () => {
+    expect(getMoviesFreshness(
+      [
+        {
+          name: 'Crazy Rich Asians',
+          rating: 93
+        },
+        {
+          name: 'Skyscraper',
+          rating: 46
+        },
+        {
+          name: 'Leave No Trace',
+          rating: 100
+        },
+        {
+          name: 'White Boy Rick',
+          rating: 60
+        }
+      ]
+    )).toEqual(
+      [
+        {
+          name: 'Crazy Rich Asians',
+          rating: 93,
+          label: 'certified fresh'
+        },
+        {
+          name: 'Skyscraper',
+          rating: 46,
+          label: 'rotten'
+        },
+        {
+          name: 'Leave No Trace',
+          rating: 100,
+          label: 'certified fresh'
+        },
+        {
+          name: 'White Boy Rick',
+          rating: 60,
+          label: 'fresh'
+        }
+      ]
+    );
+  });
+
+});
+
